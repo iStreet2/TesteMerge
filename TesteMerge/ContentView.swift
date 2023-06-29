dadb83b5af4797cb9544743f5ea0a95427060b45
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-06-29 09:22:39 -0300

    Vou bugar tudo legal

--- TesteMerge/ContentView.swift
+++ TesteMerge/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Teste Merge")
+            Text("Vou bugar tudo")
         }
         .padding()
     }
