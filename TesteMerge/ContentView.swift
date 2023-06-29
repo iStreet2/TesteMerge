a908d8dbf9dac9bd6538e21c878f4b37e828f8b8
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-06-29 09:23:42 -0300

    hehehehehe

--- TesteMerge/ContentView.swift
+++ TesteMerge/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Teste Merge")
             Text("Vou bugar tudo")
+            Text("hehehhehehehe")
         }
         .padding()
     }
