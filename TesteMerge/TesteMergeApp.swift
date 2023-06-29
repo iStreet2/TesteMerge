5e359a6193df9d32e3119db80f585e6822b1b1b0
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-06-29 09:17:05 -0300

    Initial Commit

---
+++ TesteMerge/TesteMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteMergeApp.swift
+//  TesteMerge
+//
+//  Created by Gabriel Vicentin Negro on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TesteMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
