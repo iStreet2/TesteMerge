1b9ff2d9a2cd034d826801e161eb1239002c0804
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-06-29 13:45:21 -0300

    Oi

--- TesteMerge/ContentView.swift
+++ TesteMerge/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello World")
             Text("aaaaa")
+            Text("Oi")
         }
         .padding()
     }
